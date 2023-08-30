1de914caf5d94ebe2d44d27df5262391dba5bccc
syedasadiya28 <139968182+syedasadiya28@users.noreply.github.com>
2023-08-30 12:53:23 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "my_pets" {
-  filename = "pets.txt"
-  content  = "I love pets!"
-}
-resource "random_pet" "my-pet" {
-  prefix    = "MR"
-  separator = "."
-  length    = "1"
-}
